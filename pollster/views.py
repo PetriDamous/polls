d7c6fc88603c3492d52c956c31e91e3c52d2faad
dwilliams64 <dwilliams_64@yahoo.com>
2021-01-27 18:12:22 -0600

    setup data for views and setup detail and index templates

--- pollster/views.py
+++ pollster/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+# from django.template import loader
+
+from .models import Question, Choice
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Yo, you are in the polls app!!!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'pollster/index.html', context)
 
-def frog(request):
-    return HttpResponse('ducktales')
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    choice = question.choice_set.all()
+    return render(request, 'pollster/detail.html', {'question': question, 'choice': choice})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
