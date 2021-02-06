72bb4d3ee2f4d22cf86df12fb975a31e1f9e3476
dwilliams64 <dwilliams_64@yahoo.com>
2021-02-06 00:50:04 -0600

    styled project, updated database with new questions, created about page and landing page, created templates

--- polls/urls.py
+++ polls/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [    
     path('admin/', admin.site.urls),
-    path('pollster/', include('pollster.urls'))
+    path('pollster/', include('pollster.urls')),
+    path('', include('pages.urls'))
 ]
